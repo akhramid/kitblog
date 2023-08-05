a32031814c504e2564724a3d2540608fd532d341
akhramid <aramdhan.pamuji@gmail.com>
2023-08-05 13:50:33 +0700

    ✨ edit home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="font-gray-600 text-center font-sans text-4xl font-bold md:text-left">
+	Welcome to KitBlog
+</h1>
+<p class="font-gray-300 text-center font-sans text-2xl md:text-left">Svelte Kit Markdown Blog</p>
